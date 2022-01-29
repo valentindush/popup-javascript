0d661168744393b94ed66600aa70fc4e8cfe963f
Valentin <93974733+valentindush@users.noreply.github.com>
2022-01-29 13:14:55 +0200

    maki it cool

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle(){
+    const container = document.getElementById('card')
+    const popup = document.getElementById('popup')
+
+    container.classList.toggle('active')
+    popup.classList.toggle('active')
+}
